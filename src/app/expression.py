511aee2b99d9556de69110d18e5ba3b2811b855e
Takeshi KIMURA <kittykimura@gmail.com>
2019-03-10 03:08:58 +0900

    add expression.py: most fundamental expression.

---
+++ src/app/expression.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import json
+from pprint import pprint
+
+class ExpressionGenerator(object):
+    def __init__(self):
+        pass
+    
+    def codegen(self):
+        js = {}
+        js['Who creates MW?'] = 'Machine'
+        js['MW Creator name'] = 'class ExpressionGenerator'
+
+        jsmw = js['MW CU'] = {}
+        
+        jsmw[r'@entry'] = [{'ast': 'assign',
+                            'LHS': {'ast': 'lvalue',
+                                    'type number': -64.5,
+                                    'name': 'v'},
+                            'RHS': {'ast': 'plus',
+                                    'type number': -64.5,
+                                    'LHS': {'ast': 'fvalue',
+                                            'type number': -64.5,
+                                            'value': 1.0},
+                                    'RHS': {'ast': 'fvalue',
+                                            'type number': -64.5,
+                                            'value': 2.0}}}]
+
+        print('code json:')
+        pprint(js)
+
+        return json.dumps(js)
